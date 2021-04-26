dc7b494a3e84774359742c1a1a4c0c28fbae9767
payalpatra <payalpatra105@gmail.com>
2021-04-26 20:02:03 +0530

    Fixed Data Issues

--- frontend/src/Data/hairloss.js
+++ frontend/src/Data/hairloss.js
@@ -5,20 +5,20 @@ const hairloss = [
     {
         id: "1",
         imgsrc: Biotin,
-        Name: "Biotin",
-        Indications: "Biotin is a dietary supplement prescribed to prevent and treat a rare biotin deficiency which is associated with malnutrition, pregnancy, long-term tube feeding and rapid weight loss, It assists in various biochemical pathways as a cofactor that break down the body’s carbohydrate, protein and fat, Biotin is used for conditions like hair loss, brittle nails, conjunctivitis, and seborrheic dermatitis in children, It is also used in some metabolic disorders. However, its effectiveness in these conditions has not been established.",
+        title: "Biotin",
+        indication: "Biotin is a dietary supplement prescribed to prevent and treat a rare biotin deficiency which is associated with malnutrition, pregnancy, long-term tube feeding and rapid weight loss, It assists in various biochemical pathways as a cofactor that break down the body’s carbohydrate, protein and fat, Biotin is used for conditions like hair loss, brittle nails, conjunctivitis, and seborrheic dermatitis in children, It is also used in some metabolic disorders. However, its effectiveness in these conditions has not been established.",
         Dosage: "As directed by physician",
-        SideEffects: "• Biotin is usually safe and very well-tolerated, Adverse effects include allergic reactions or mild upset stomach, Skin reactions such as urticaria, itchiness, redness or pain at injection site may occur after the administration of injections.",
-        Price: "₹350.00",
+        sideEffects: "• Biotin is usually safe and very well-tolerated, Adverse effects include allergic reactions or mild upset stomach, Skin reactions such as urticaria, itchiness, redness or pain at injection site may occur after the administration of injections.",
+        price: 350.00,
     },
     {
         id: "2",
         imgsrc: min,
-        Name: "Minoxidil Topical ",
-        Indications: "This medication is a vasodilator, prescribed for hair loss.",
+        title: "Minoxidil Topical ",
+        indication: "This medication is a vasodilator, prescribed for hair loss.",
         Dosage: "Topical- Male pattern baldness: As 2% or 5% solution: Apply 1 mL twice daily.",
-        SideEffects: "Scalp itching, dryness, scaling, irritation and burning.",
-        Price: "₹705.00",
+        sideEffects: "Scalp itching, dryness, scaling, irritation and burning.",
+        price: 705.00,
     },
 ]
 export default hairloss;
